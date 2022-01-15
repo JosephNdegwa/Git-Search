cb872bb092b1b57a69f9e2c9c2dbeaad3c3d7d2d
josephndegwa <kariukindegwa98@gmail.com>
2022-01-15 13:33:47 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepositComponent } from './reposit/reposit.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'reposit', component: RepositComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RepositComponent } from './reposit/reposit.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     RepositComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    NavbarComponent
     
   ],
 
