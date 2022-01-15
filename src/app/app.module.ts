b4f9a4503330cb01b3d8e8b5f4118e2b6426e0aa
josephndegwa <kariukindegwa98@gmail.com>
2022-01-15 12:07:33 +0300

    add form logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositComponent } from './reposit/reposit.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    RepositComponent
+    RepositComponent,
+    SearchFormComponent
     
   ],
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
 
   providers: [],
