bdcd26c2470b0b6b13e02f910db42a7f2c8dd207
josephndegwa <kariukindegwa98@gmail.com>
2022-01-16 20:53:07 +0300

    put environment into gitignore

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -13,5 +13,60 @@ export class UserServiceService {
   foundUser!: User;
   allRepo!: Repo;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.foundUser = new User("","","","",0,0,0,"",new Date);
+    this.allRepo = new Repo("","","",new Date,0,0,"");
+  }
+
+  searchUSer(searchName: string) {
+   
+    interface Responce {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.http.get<Responce>('https://api.github.com/users/'+searchName+'?access_token='+environment.apiKey).toPromise().then(
+        (result) => {
+          this.foundUser = result;
+          console.log(this.foundUser);
+         // resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+  }
+  getReopo(searchName){
+    interface Repos{
+      name:string;
+      html_url:string;
+      description:string;
+      forks:number;
+      watchers_count:number;
+      language:string;
+      created_at:Date;
+    }
+    return new Promise((resolve,reject)=>{
+      this.http.get<Repos>('https://api.github.com/users/'+searchName+"/repos?order=created&sort=asc?access_token="+environment.apiKey).toPromise().then(
+        (results) => {
+          this.allRepo = results;
+          //resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+  }
 }
